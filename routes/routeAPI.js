a77ad7db985ea86aeb0b861590df7f10d8fa84df
Alisha Panday <panday.alisha90@gmail.com>
2021-02-07 22:39:31 +0800

    getNotes fn and route for api post added

---
+++ routes/routeAPI.js
@@ -0,0 +1,40 @@
+
+const fs = require("fs");
+const path = require("path");
+var notesInfo = getNotes();
+
+function getNotes() {
+  //mention file encoding UTF-8 to ignore buffer data
+  let data = fs.readFileSync("./db/db.json", "UTF-8");
+  // console.log(data);
+  let notes = JSON.parse(data);
+  // console.log(notes);
+  for (let i = 0; i < notes.length; i++) {
+    notes[i].id = "" + i;
+    console.log(notes);
+  }
+  return notes;
+}
+
+module.exports = (app) => {
+
+  app.get("/api/notes", (req,res) =>{
+      notesInfo = getNotes();
+      //sends JSON response by converting noteData to JSON string using JSON stringfy() method
+      res.json(notesInfo);
+  });
+
+  app.get("api/notes", (req,res)=>{
+    console.log(req.body);
+    notesInfo.push(req.body);
+    res.json(true);
+    
+  });
+
+  app.post("/api/notes", (req,res) =>{
+    notesInfo.push(req.body);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notesInfo), "UTF-8");
+    res.json(true);
+  });
+
+};
